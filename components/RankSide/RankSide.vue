04560b097ebbf7b2c70ea457d3929de6a00d1848
NguyenDuyHieu987 <vaicut6941@gmail.com>
2023-06-07 10:43:22 +0700

    add

--- components/RankSide/RankSide.vue
+++ components/RankSide/RankSide.vue
@@ -83,72 +83,77 @@ const allTabs = ref<tab[]>([
 ]);
 
 const getData = async (activeKey: string) => {
-  loading.value = true;
-  switch (activeKey) {
-    case 'day':
-      await useAsyncData(`ranking/all/1`, () => getRanking(1))
-        .then((movieRespone: any) => {
-          rankData.value = movieRespone.data.value?.results;
-          setTimeout(() => {
+  if (activeTab.value != activeKey) {
+    loading.value = true;
+    switch (activeKey) {
+      case 'day':
+        await useAsyncData(`ranking/all/1`, () => getRanking(1))
+          .then((movieRespone: any) => {
+            rankData.value = movieRespone.data.value?.results;
+          })
+          .catch((e) => {
+            if (axios.isCancel(e)) return;
+          })
+          .finally(() => {
             loading.value = false;
-          }, 1500);
-        })
-        .catch((e) => {
-          if (axios.isCancel(e)) return;
-        });
+          });
 
-      activeTab.value = activeKey;
-      break;
-    case 'week':
-      await useAsyncData(`ranking/all/2`, () => getRanking(2))
-        .then((movieRespone: any) => {
-          rankData.value = movieRespone.data.value?.results;
-
-          setTimeout(() => {
+        break;
+      case 'week':
+        await useAsyncData(`ranking/all/2`, () => getRanking(2))
+          .then((movieRespone: any) => {
+            rankData.value = movieRespone.data.value?.results;
+          })
+          .catch((e) => {
+            if (axios.isCancel(e)) return;
+          })
+          .finally(() => {
             loading.value = false;
-          }, 1500);
-        })
-        .catch((e) => {
-          if (axios.isCancel(e)) return;
-        });
+          });
 
-      activeTab.value = activeKey;
-      break;
-    case 'month':
-      await useAsyncData(`ranking/all/3`, () => getRanking(3))
-        .then((movieRespone: any) => {
-          rankData.value = movieRespone.data.value?.results;
-
-          setTimeout(() => {
+        break;
+      case 'month':
+        await useAsyncData(`ranking/all/3`, () => getRanking(3))
+          .then((movieRespone: any) => {
+            rankData.value = movieRespone.data.value?.results;
+          })
+          .catch((e) => {
+            if (axios.isCancel(e)) return;
+          })
+          .finally(() => {
             loading.value = false;
-          }, 1500);
-        })
-        .catch((e) => {
-          if (axios.isCancel(e)) return;
-        });
+          });
 
-      activeTab.value = activeKey;
-      break;
-    case 'all':
-      await useAsyncData(`ranking/all/4`, () => getRanking(4))
-        .then((movieRespone: any) => {
-          rankData.value = movieRespone.data.value?.results;
-
-          setTimeout(() => {
+        break;
+      case 'all':
+        await useAsyncData(`ranking/all/4`, () => getRanking(4))
+          .then((movieRespone: any) => {
+            rankData.value = movieRespone.data.value?.results;
+          })
+          .catch((e) => {
+            if (axios.isCancel(e)) return;
+          })
+          .finally(() => {
             loading.value = false;
-          }, 1500);
-        })
-        .catch((e) => {
-          if (axios.isCancel(e)) return;
-        });
+          });
 
-      activeTab.value = activeKey;
-      break;
+        break;
+    }
+    activeTab.value = activeKey;
   }
 };
 
-onBeforeMount(() => {
-  getData(activeTab.value);
+onBeforeMount(async () => {
+  await useAsyncData(`ranking/all/1`, () => getRanking(1))
+    .then((movieRespone: any) => {
+      rankData.value = movieRespone.data.value?.results;
+      setTimeout(() => {
+        loading.value = false;
+      }, 1500);
+    })
+    .catch((e) => {
+      if (axios.isCancel(e)) return;
+    });
 });
 
 const handleTabClick = (activeKey: any) => {
