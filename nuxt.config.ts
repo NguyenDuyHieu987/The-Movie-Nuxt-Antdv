ab1611bcdc86671d237264efcec12837f80d20a2
NguyenDuyHieu987 <vaicut6941@gmail.com>
2023-07-01 00:39:59 +0700

    add

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -164,7 +164,7 @@ export default defineNuxtConfig({
     { src: '@/plugins/loadFacebookSdk', mode: 'client' },
   ],
   nitro: {
-    preset: 'cloudflare-pages',
+    preset: 'static',
     prerender: {
       routes: [
         '/',
