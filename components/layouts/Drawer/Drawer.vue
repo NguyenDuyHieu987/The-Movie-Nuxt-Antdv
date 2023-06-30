ff7662ef765a4b95951c389da47c7b533328160d
NguyenDuyHieu987 <vaicut6941@gmail.com>
2023-06-30 13:40:36 +0700

    add

--- components/layouts/Drawer/Drawer.vue
+++ components/layouts/Drawer/Drawer.vue
@@ -1,5 +1,5 @@
 <template>
-  <!-- <a-drawer
+  <a-drawer
     class="menu-drawer"
     v-model:visible="openDrawer"
     placement="left"
@@ -25,37 +25,38 @@
       </div>
     </template>
     <TheMenu />
-  </a-drawer> -->
+  </a-drawer>
 
-  <el-drawer
-    v-if="openDrawer"
-    class="menu-drawer"
-    v-model="openDrawer"
-    direction="ltr"
-    placement="left"
-    :show-close="false"
-  >
-    <template #header>
-      <div class="user-header">
-        <div class="user-image-container" v-if="!collapsed && isLogin">
-          <nuxt-img
-            class="avatar"
-            :src="
-              !isNaN(+userAccount?.avatar)
-                ? getImage(`account${userAccount?.avatar}.jpg`, 'user_avatar')
-                : userAccount?.avatar
-            "
-            alt=""
-          />
+  <!-- <ClientOnly>
+    <el-drawer
+      class="menu-drawer"
+      v-model="openDrawer"
+      direction="ltr"
+      placement="left"
+      :show-close="false"
+    >
+      <template #header>
+        <div class="user-header">
+          <div class="user-image-container" v-if="!collapsed && isLogin">
+            <nuxt-img
+              class="avatar"
+              :src="
+                !isNaN(+userAccount?.avatar)
+                  ? getImage(`account${userAccount?.avatar}.jpg`, 'user_avatar')
+                  : userAccount?.avatar
+              "
+              alt=""
+            />
+          </div>
+
+          <h4>
+            {{ userAccount?.username }}
+          </h4>
         </div>
-
-        <h4>
-          {{ userAccount?.username }}
-        </h4>
-      </div>
-    </template>
-    <TheMenu />
-  </el-drawer>
+      </template>
+      <TheMenu />
+    </el-drawer>
+  </ClientOnly> -->
 </template>
 
 <script setup lang="ts">
