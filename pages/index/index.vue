26645f116a3f173f765bd47aac33b6293964784d
NguyenDuyHieu987 <vaicut6941@gmail.com>
2023-07-12 17:06:01 +0700

    add

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -64,7 +64,16 @@
           @click="handleLoadMoreRecommend"
         >
           <template #icon>
-            <Icon name="ic:baseline-add" />
+            <!-- <Icon name="ic:baseline-add" /> -->
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="2rem"
+              height="2rem"
+              viewBox="0 0 24 24"
+            >
+              <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" />
+            </svg>
           </template>
           {{ loadMoreRecommend ? 'Đang tải...' : 'Tải thêm' }}
         </a-button>
