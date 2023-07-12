8b92a7e4a880a2de9f3d8ad9fe3b28adfcca1c01
NguyenDuyHieu987 <vaicut6941@gmail.com>
2023-07-12 12:09:48 +0700

    add

--- plugins/antd.ts
+++ plugins/antd.ts
@@ -2,6 +2,7 @@ import Antd from 'ant-design-vue';
 // import 'ant-design-vue/dist/antd.min.css';
 
 export default defineNuxtPlugin((nuxtApp) => {
-  // nuxtApp.hook('app:created', () => {});
-  nuxtApp.vueApp.use(Antd);
+  nuxtApp.hook('app:created', () => {
+    nuxtApp.vueApp.use(Antd);
+  });
 });
