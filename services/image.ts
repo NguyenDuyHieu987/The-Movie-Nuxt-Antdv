81907124a21caa5083a32a4351621edf7bb9e7e4
NguyenDuyHieu987 <vaicut6941@gmail.com>
2023-07-23 12:19:03 +0700

    add

--- services/image.ts
+++ services/image.ts
@@ -1,8 +1,7 @@
 import { makeRequest } from './makeRequest';
 
-// const URL_API_IMAGE = 'http://127.0.0.1:5001';
 const URL_API_IMAGE = 'https://img.phimhay247.site';
-const URL_API_IMAGE1 = 'https://ik.imagekit.io/8toa5f2rp';
+// const URL_API_IMAGE1 = 'https://ik.imagekit.io/8toa5f2rp';
 // const URL_API_IMAGE1 = 'https://res.cloudinary.com/dvbhjlrdf/image/upload';
 
 // export function getImage(path: string, type: string, crop: string = '') {
@@ -12,6 +11,10 @@ const URL_API_IMAGE1 = 'https://ik.imagekit.io/8toa5f2rp';
 // }
 
 export function getImage(path: string, type: string, crop: string = '') {
+  const nuxtConfig = useRuntimeConfig();
+
+  const URL_API_IMAGE1 = nuxtConfig.app.serverImageUrl;
+
   if (crop?.length == 0)
     return path ? `${URL_API_IMAGE1}/images/${type}/${path}` : ' ';
 
