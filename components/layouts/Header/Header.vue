3c7fe1301a06030091ee01fc5a50736cb63f0ce3
NguyenDuyHieu987 <vaicut6941@gmail.com>
2023-07-24 23:58:06 +0700

    add

--- components/layouts/Header/Header.vue
+++ components/layouts/Header/Header.vue
@@ -187,7 +187,6 @@
             class="dropdown-notification"
             overlayClassName="dropdown-notification header"
             placement="bottomRight"
-            destroyPopupOnHide
           >
             <span v-if="isLogin" class="ant-dropdown-link">
               <a-badge :count="3" :overflow-count="9">
@@ -257,7 +256,6 @@
             class="dropdown-account"
             overlayClassName="dropdown-account header"
             placement="bottomRight"
-            destroyPopupOnHide
           >
             <span class="ant-dropdown-link">
               <!-- <UserOutlined />
