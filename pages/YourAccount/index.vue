7d8c78f831dbbee5e598460153cc527665cc8553
NguyenDuyHieu987 <vaicut6941@gmail.com>
2023-07-26 10:22:27 +0700

    add

--- pages/YourAccount/index.vue
+++ pages/YourAccount/index.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="your-account padding-content">
-    <h2>Username: {{ store.$state.userAccount?.username }}</h2>
-    <h3>Email: {{ store.$state.userAccount?.email }}</h3>
-    <h3>Fullname: {{ store.$state.userAccount?.full_name }}</h3>
+    <div class="your-account-container">
+      <div class="your-account-header">
+        <h2>Tài khoản</h2>
+        <div class="join-since">
+          <span>{{ 'Tham gia từ ' + userAccount?.created_at }}</span>
+        </div>
+      </div>
+      <h2>Username: {{ userAccount?.username }}</h2>
+      <h3>Email: {{ userAccount?.email }}</h3>
+      <h3>Fullname: {{ userAccount?.full_name }}</h3>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { storeToRefs } from 'pinia';
+
 const store = useStore();
+const { userAccount } = storeToRefs(store);
 
 useHead({
   title: 'Profile - Hồ sơ | Phimhay247',
