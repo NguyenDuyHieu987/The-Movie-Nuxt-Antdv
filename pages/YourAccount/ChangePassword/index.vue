02356b916b557c571105b0d655d0bdfff8005e45
NguyenDuyHieu987 <vaicut6941@gmail.com>
2023-07-28 04:08:30 +0700

    add

--- pages/YourAccount/ChangePassword/index.vue
+++ pages/YourAccount/ChangePassword/index.vue
@@ -3,72 +3,86 @@
     <div v-show="isLogin" class="password-container">
       <Transition appear name="slide-left">
         <div v-show="showAnimation">
-          <div class="changePass-header">
-            <h1>Đổi mật khẩu của bạn</h1>
-            <p>
-              Để bảo vệ tài khoản bạn nên đặt một mật khẩu duy nhất dài ít nhất
-              6 ký tự.
-            </p>
+          <div v-show="!isChangePassword">
+            <div class="changePass-header">
+              <h1>Đổi mật khẩu của bạn</h1>
+              <p>
+                Để bảo vệ tài khoản bạn nên đặt một mật khẩu duy nhất dài ít
+                nhất 6 ký tự.
+              </p>
+            </div>
+            <a-form
+              :model="formChangePassword"
+              :rules="rules"
+              name="change-password-form"
+              class="form-change-password"
+              @finish="handleSubmit"
+              hideRequiredMark
+            >
+              <a-form-item
+                label="Mật khẩu cũ"
+                name="oldPassword"
+                :rules="[
+                  {
+                    required: true,
+                    message: 'Vui lòng nhập mật khẩu!',
+                    trigger: ['change', 'blur'],
+                  },
+                ]"
+                has-feedback
+              >
+                <a-input-password
+                  v-model:value="formChangePassword.oldPassword"
+                  placeholder="Mật khẩu cũ..."
+                >
+                </a-input-password>
+              </a-form-item>
+
+              <a-form-item label="Mật khẩu mới" name="newPassword" has-feedback>
+                <a-input-password
+                  v-model:value="formChangePassword.newPassword"
+                  placeholder="Mật khẩu mới..."
+                >
+                </a-input-password>
+              </a-form-item>
+
+              <a-form-item
+                label="Xác nhận lại"
+                name="confirmNewPassword"
+                has-feedback
+              >
+                <a-input-password
+                  v-model:value="formChangePassword.confirmNewPassword"
+                  placeholder="Xác nhận lại mật khẩu..."
+                >
+                </a-input-password>
+              </a-form-item>
+
+              <a-form-item>
+                <a-button
+                  class="submit-form-button click-active"
+                  type="primary"
+                  html-type="submit"
+                  size="large"
+                  :loading="laoding"
+                >
+                  Đổi mật khẩu
+                </a-button>
+              </a-form-item>
+            </a-form>
           </div>
-          <a-form
-            :model="formState"
-            :rules="rules"
-            name="change-password-form"
-            class="form-change-password"
-            @finish="handleSubmit"
-            hideRequiredMark
-          >
-            <a-form-item
-              label="Mật khẩu cũ"
-              name="oldPassword"
-              :rules="[
-                {
-                  required: true,
-                  message: 'Vui lòng nhập mật khẩu!',
-                  trigger: ['change', 'blur'],
-                },
-              ]"
-              has-feedback
-            >
-              <a-input-password
-                v-model:value="formState.oldPassword"
-                placeholder="Mật khẩu cũ..."
-              >
-              </a-input-password>
-            </a-form-item>
 
-            <a-form-item label="Mật khẩu mới" name="newPassword" has-feedback>
-              <a-input-password
-                v-model:value="formState.newPassword"
-                placeholder="Mật khẩu mới..."
-              >
-              </a-input-password>
-            </a-form-item>
-
-            <a-form-item
-              label="Xác nhận lại"
-              name="confirmNewPassword"
-              has-feedback
-            >
-              <a-input-password
-                v-model:value="formState.confirmNewPassword"
-                placeholder="Xác nhận lại mật khẩu..."
-              >
-              </a-input-password>
-            </a-form-item>
-
-            <a-form-item>
-              <a-button
-                class="submit-form-button click-active"
-                type="primary"
-                html-type="submit"
-                size="large"
-                :loading="laoding"
-              >
-                Đổi mật khẩu
-              </a-button>
-            </a-form-item>
-          </a-form>
+          <VerifyForm
+            v-model:isShowForm="isChangePassword"
+            :email="formChangePassword.email"
+            :jwtVerifyEmail="jwtVerifyEmail"
+            :otpExpOffset="otpExpOffset"
+            v-model:loadingResend="loadingResend"
+            v-model:disabled_countdown="disabled_countdown"
+            v-model:loadingVerify="loadingVerify"
+            @onVerify="handleVerify"
+            @onResend="handleResendVerifyEmail"
+          />
         </div>
       </Transition>
     </div>
@@ -77,6 +91,7 @@
 </template>
 
 <script setup lang="ts">
+import VerifyForm from '~/components/VerifyForm/VerifyForm.vue';
 import RequireAuth from '@/components/RequireAuth/RequireAuth.vue';
 import { storeToRefs } from 'pinia';
 
@@ -89,16 +104,24 @@ definePageMeta({
 const store: any = useStore();
 const { isLogin } = storeToRefs<any>(store);
 const laoding = ref<boolean>(false);
-const formState = reactive<{
+const formChangePassword = reactive<{
+  email: string;
   oldPassword: string;
   newPassword: string;
   confirmNewPassword: string;
 }>({
+  email: store.userAccount?.email,
   oldPassword: '',
   newPassword: '',
   confirmNewPassword: '',
 });
 const showAnimation = ref<boolean>(false);
+const loadingVerify = ref<boolean>(false);
+const isChangePassword = ref<boolean>(false);
+const jwtVerifyEmail = ref<string>('');
+const disabled_countdown = ref<boolean>(true);
+const loadingResend = ref<boolean>(false);
+const otpExpOffset = ref<number>(60);
 
 useHead({
   title: 'Lịch sử giao dịch - Hóa đơn - Thanh toán | Phimhay247',
@@ -128,18 +151,18 @@ onBeforeMount(() => {
 const disabled = computed<boolean>((): boolean => {
   return !(
     (
-      formState.oldPassword &&
-      formState.newPassword &&
-      formState.confirmNewPassword
+      formChangePassword.oldPassword &&
+      formChangePassword.newPassword &&
+      formChangePassword.confirmNewPassword
     )
     // &&
-    // formState.oldPassword != formState.newPassword &&
-    // formState.newPassword == formState.confirmNewPassword
+    // formChangePassword.oldPassword != formChangePassword.newPassword &&
+    // formChangePassword.newPassword == formChangePassword.confirmNewPassword
   );
 });
 
 const checkNewPassword = async (_rule: any, value: string) => {
-  if (value == formState.oldPassword) {
+  if (value == formChangePassword.oldPassword) {
     return Promise.reject('Mật khẩu mới không được trùng với mật khẩu cũ!');
   } else {
     return Promise.resolve();
@@ -147,7 +170,7 @@ const checkNewPassword = async (_rule: any, value: string) => {
 };
 
 const checkConfirmNewPassword = async (_rule: any, value: string) => {
-  if (value !== formState.newPassword) {
+  if (value !== formChangePassword.newPassword) {
     return Promise.reject('Mật khẩu nhập lại không khớp!');
   } else {
     return Promise.resolve();
@@ -185,8 +208,12 @@ const rules = {
 };
 
 const handleSubmit = () => {
-  alert('G');
+  isChangePassword.value = true;
 };
+
+const handleVerify = () => {};
+
+const handleResendVerifyEmail = () => {};
 </script>
 
 <style lang="scss" src="./ChangePasswordPage.scss"></style>
