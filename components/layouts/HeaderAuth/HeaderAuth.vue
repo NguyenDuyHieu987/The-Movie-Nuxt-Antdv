2e48a44296fe96fc65331575b6fd82b74a8daa0f
NguyenDuyHieu987 <vaicut6941@gmail.com>
2023-07-30 18:37:33 +0700

    add

--- components/layouts/HeaderAuth/HeaderAuth.vue
+++ components/layouts/HeaderAuth/HeaderAuth.vue
@@ -15,9 +15,17 @@
     </div>
 
     <div class="right-header">
-      <NuxtLink :to="route.path == '/login' ? '/signup' : '/login'">
-        <span v-if="route.path == '/login'"> Đăng ký ngay</span>
-        <span v-if="route.path == '/signup'"> Đăng nhập</span>
+      <div class="signup-suggest">
+        <span v-if="route.path == '/login'" class="signup-note">
+          Chưa có tài khoản?
+        </span>
+        <NuxtLink v-if="route.path == '/login'" to="/signup">
+          <span> Đăng ký ngay</span>
+        </NuxtLink>
+      </div>
+
+      <NuxtLink v-if="route.path == '/signup'" to="/login">
+        Đăng nhập
       </NuxtLink>
     </div>
   </header>
