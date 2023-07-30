027796d10a85f9d17ee215c1df3e32378a77569a
NguyenDuyHieu987 <vaicut6941@gmail.com>
2023-07-30 21:38:54 +0700

    add

--- pages/YourAccount/ChangeEmail/index.vue
+++ pages/YourAccount/ChangeEmail/index.vue
@@ -34,7 +34,7 @@
                     d="M12 1L3 5v6c0 5.55 3.84 10.74 9 12c5.16-1.26 9-6.45 9-12V5l-9-4zm7 10c0 4.52-2.98 8.69-7 9.93c-4.02-1.24-7-5.41-7-9.93V6.3l7-3.11l7 3.11V11zm-11.59.59L6 13l4 4l8-8l-1.41-1.42L10 14.17z"
                   />
                 </svg>
-                <h1>Trước tiên, Hãy xác nhận Email là của bạn</h1>
+                <h1>Trước tiên, Hãy xác nhận Email này là của bạn</h1>
                 <p>
                   Trước khi cấp phép thực hiện bất kỳ thay đổi nào, chúng tôi
                   cần đảm bảo đây là bạn.
