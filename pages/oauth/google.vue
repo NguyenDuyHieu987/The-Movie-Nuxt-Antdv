fbe6322e5c766dbd2656e0999616d672df6572ce
NguyenDuyHieu987 <vaicut6941@gmail.com>
2023-07-31 19:42:12 +0700

    add

--- pages/oauth/google.vue
+++ pages/oauth/google.vue
@@ -1,5 +1,103 @@
 <template>
-  <!-- <div class="oauth padding-content"></div> -->
+  <div class="oauth-google">
+    <div class="icon-loading">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="4rem"
+        height="4rem"
+        viewBox="0 0 24 24"
+      >
+        <circle cx="12" cy="2" r="0">
+          <animate
+            attributeName="r"
+            begin="0"
+            calcMode="spline"
+            dur="1s"
+            keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+            repeatCount="indefinite"
+            values="0;2;0;0"
+          />
+        </circle>
+        <circle cx="12" cy="2" r="0" transform="rotate(45 12 12)">
+          <animate
+            attributeName="r"
+            begin="0.125s"
+            calcMode="spline"
+            dur="1s"
+            keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+            repeatCount="indefinite"
+            values="0;2;0;0"
+          />
+        </circle>
+        <circle cx="12" cy="2" r="0" transform="rotate(90 12 12)">
+          <animate
+            attributeName="r"
+            begin="0.25s"
+            calcMode="spline"
+            dur="1s"
+            keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+            repeatCount="indefinite"
+            values="0;2;0;0"
+          />
+        </circle>
+        <circle cx="12" cy="2" r="0" transform="rotate(135 12 12)">
+          <animate
+            attributeName="r"
+            begin="0.375s"
+            calcMode="spline"
+            dur="1s"
+            keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+            repeatCount="indefinite"
+            values="0;2;0;0"
+          />
+        </circle>
+        <circle cx="12" cy="2" r="0" transform="rotate(180 12 12)">
+          <animate
+            attributeName="r"
+            begin="0.5s"
+            calcMode="spline"
+            dur="1s"
+            keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+            repeatCount="indefinite"
+            values="0;2;0;0"
+          />
+        </circle>
+        <circle cx="12" cy="2" r="0" transform="rotate(225 12 12)">
+          <animate
+            attributeName="r"
+            begin="0.625s"
+            calcMode="spline"
+            dur="1s"
+            keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+            repeatCount="indefinite"
+            values="0;2;0;0"
+          />
+        </circle>
+        <circle cx="12" cy="2" r="0" transform="rotate(270 12 12)">
+          <animate
+            attributeName="r"
+            begin="0.75s"
+            calcMode="spline"
+            dur="1s"
+            keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+            repeatCount="indefinite"
+            values="0;2;0;0"
+          />
+        </circle>
+        <circle cx="12" cy="2" r="0" transform="rotate(315 12 12)">
+          <animate
+            attributeName="r"
+            begin="0.875s"
+            calcMode="spline"
+            dur="1s"
+            keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+            repeatCount="indefinite"
+            values="0;2;0;0"
+          />
+        </circle>
+      </svg>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -29,7 +127,7 @@ onBeforeMount(() => {
   }
 
   loading.value = true;
-  store.loadingAppInstance.start();
+  // store.loadingAppInstance.start();
 
   loginGoogle({
     accessToken: access_token,
@@ -101,7 +199,7 @@ onBeforeMount(() => {
       loading.value = false;
 
       setTimeout(() => {
-        store.loadingAppInstance.finish();
+        // store.loadingAppInstance.finish();
       }, 300);
     });
 });
@@ -111,3 +209,19 @@ onBeforeMount(() => {
 //   immediate: true,
 // });
 </script>
+
+<style lang="scss" scoped>
+.oauth-google {
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  svg {
+    fill: var(--app-background-color);
+    width: 4rem;
+    height: 4rem;
+  }
+}
+</style>
