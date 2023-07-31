30b99eee988ac58572627898cd244ba0c2c1ef4b
NguyenDuyHieu987 <vaicut6941@gmail.com>
2023-08-01 01:17:12 +0700

    add

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -98,14 +98,14 @@ export default defineNuxtConfig({
           type: 'text/javascript',
           async: true,
         },
-        {
-          hid: 'gtmHead',
-          innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','${process.env.GOOGLE_TAG_MANAGER_ID}');`,
-        },
+        // {
+        //   hid: 'gtmHead',
+        //   innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        //   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        //   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        //   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        //   })(window,document,'script','dataLayer','${process.env.GOOGLE_TAG_MANAGER_ID}');`,
+        // },
       ],
     },
     pageTransition: { name: 'page', mode: 'out-in', appear: true },
