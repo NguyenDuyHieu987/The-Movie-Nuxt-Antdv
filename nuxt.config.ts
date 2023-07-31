9e2d2b54b8c3760ff636f0290f95a3cd7fe07684
NguyenDuyHieu987 <vaicut6941@gmail.com>
2023-07-31 19:42:54 +0700

    add

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -303,9 +303,9 @@ export default defineNuxtConfig({
     '/info-tv/**': { ssr: false, swr: true, prerender: true, static: true },
     '/play-movie/**': { ssr: false, swr: true, prerender: true, static: true },
     '/play-tv/**': { ssr: false, swr: true, prerender: true, static: true },
-    '/oauth': { swr: true, prerender: true, static: true },
-    '/login': { swr: true, prerender: true, static: true },
-    '/signup': { swr: true, prerender: true, static: true },
+    '/oauth': { prerender: true, static: true },
+    '/login': { prerender: true, static: true },
+    '/signup': { prerender: true, static: true },
     '/upgrade/plans': { swr: true, prerender: true, static: true },
     '/upgrade/payment': {
       ssr: false,
