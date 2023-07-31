61951e01f1ee8d6736abc8d1a9ecad4add325ab9
NguyenDuyHieu987 <vaicut6941@gmail.com>
2023-07-31 23:39:41 +0700

    add

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -4,13 +4,16 @@
     <div class="background-auth">
       <div class="image-auth desktop">
         <nuxt-img
-          :src="// getImage(`Background_Auth${image}.jpg`, '/background/auth')
-          // getImage(
-          //   'Background_1.jpg',
-          //   'background/1',
-          //   'w-' + windowWidth?.toString()
-          // )
-          'https://ik.imagekit.io/8toa5f2rp/images/background/1/Background_1.jpg'"
+          :src="
+            // getImage(`Background_Auth${image}.jpg`, '/background/auth')
+            // 'https://ik.imagekit.io/8toa5f2rp/images/background/1/Background_1.jpg'
+
+            getImage(
+              'Background_1.jpg',
+              'background/1',
+              windowWidth ? 'w-' + windowWidth?.toString() : ''
+            )
+          "
           alt=""
           loading="lazy"
         />
@@ -34,11 +37,15 @@
 
 <script setup lang="ts">
 import HeaderAuth from '~/components/layouts/HeaderAuth/HeaderAuth.vue';
-// import { getImage } from '~/services/image';
+import { getImage } from '~/services/image';
 
 const image = ref<number>(Math.floor(Math.random() * 3) + 1);
 const image_mobile = ref<number>(Math.floor(Math.random() * 3) + 1);
-const windowWidth = ref<number>(window.innerWidth);
+const windowWidth = ref<number>();
+
+onBeforeMount(() => {
+  windowWidth.value = window.innerWidth;
+});
 </script>
 
 <style lang="scss" scoped src="~/assets/style/authLayout.scss"></style>
