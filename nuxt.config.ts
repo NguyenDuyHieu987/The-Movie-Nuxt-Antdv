a113b5e352ad9bfc2c7585f4c00afbc9ba854f4e
NguyenDuyHieu987 <vaicut6941@gmail.com>
2023-08-01 13:12:00 +0700

    add

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -69,10 +69,10 @@ export default defineNuxtConfig({
         //   rel: 'stylesheet',
         //   href: '/icons/google-material-icons/css/material-icons.min.css',
         // },
-        {
-          rel: 'preload',
-          href: 'https://fonts.googleapis.com/css2?family=Lobster&family=Lobster+Two&family=Roboto+Flex:opsz,wght@8..144,100;8..144,200;8..144,300;8..144,400;8..144,500;8..144,600;8..144,700&family=Roboto:wght@100;300;400;500;700&display=swap',
-        },
+        // {
+        //   rel: 'preload',
+        //   href: 'https://fonts.googleapis.com/css2?family=Lobster&family=Lobster+Two&family=Roboto+Flex:opsz,wght@8..144,100;8..144,200;8..144,300;8..144,400;8..144,500;8..144,600;8..144,700&family=Roboto:wght@100;300;400;500;700&display=swap',
+        // },
       ],
       script: [
         // {
@@ -133,6 +133,7 @@ export default defineNuxtConfig({
   },
   experimental: {
     payloadExtraction: false,
+    viewTransition: true,
   },
   typescript: {
     strict: true,
