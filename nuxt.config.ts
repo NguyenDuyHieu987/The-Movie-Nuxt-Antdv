75e937a1937f4afdef54fc65e89d2d6b842f9220
NguyenDuyHieu987 <vaicut6941@gmail.com>
2023-08-02 01:24:48 +0700

    add

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -79,9 +79,8 @@ export default defineNuxtConfig({
           crossorigin: 'anonymous',
         },
         {
-          rel: 'preconnect',
-          href: `https://fonts.gstatic.com" crossorigin>
-          <link href="https://fonts.googleapis.com/css2?family=Lobster&family=Roboto+Flex:opsz,wght@8..144,100;8..144,200;8..144,300;8..144,400;8..144,500;8..144,600&family=Roboto:wght@100;300;400;500;700&display=swap`,
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Lobster&family=Roboto+Flex:wght@100;200;300;400;500;600&family=Roboto:wght@100;300;400;500;700&display=swap',
         },
       ],
       script: [
@@ -163,7 +162,7 @@ export default defineNuxtConfig({
     '@nuxt/image',
     'nuxt-lazy-load',
     '@nuxtjs/fontaine',
-    // '@nuxtjs/google-fonts',
+    '@nuxtjs/google-fonts',
     'nuxt-simple-sitemap',
     'nuxt-og-image',
     'nuxt-seo-experiments',
@@ -218,22 +217,13 @@ export default defineNuxtConfig({
       // See IntersectionObserver documentation
     },
   },
-  // googleFonts: {
-  //   prefetch: true,
-  //   preconnect: true,
-  //   preload: true,
-  //   display: 'swap',
-  //   useStylesheet: true,
-  //   families: {
-  //     Roboto: true,
-  //     'Josefin+Sans': true,
-  //     Lato: [100, 300],
-  //     Raleway: {
-  //       wght: [100, 400],
-  //       ital: [100],
-  //     },
-  //   },
-  // },
+  googleFonts: {
+    prefetch: true,
+    preconnect: true,
+    preload: true,
+    display: 'swap',
+    useStylesheet: true,
+  },
   fontMetrics: {
     fonts: [
       'Inter',
