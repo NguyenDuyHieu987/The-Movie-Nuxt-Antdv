e02d35584c6df1596b2c63fb1ade29adccb20d4a
NguyenDuyHieu987 <vaicut6941@gmail.com>
2023-08-03 00:10:08 +0700

    add

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -142,7 +142,6 @@ export default defineNuxtConfig({
   },
   alias: {
     '@': resolve(__dirname, './'),
-    '~': resolve(__dirname, './'),
   },
   experimental: {
     payloadExtraction: false,
@@ -155,7 +154,7 @@ export default defineNuxtConfig({
     url: 'https://phimhay247.tech',
   },
   css: [
-    // 'ant-design-vue/dist/antd.dark.min.css',
+    'ant-design-vue/dist/antd.dark.min.css',
     'element-plus/dist/index.css',
     '~/assets/style/globalStyle.scss',
     // '@fortawesome/fontawesome-svg-core/styles.css',
@@ -317,12 +316,9 @@ export default defineNuxtConfig({
   // components: true,
   vite: {
     plugins: [
-      // Components({
-      //   resolvers: [
-      //     AntDesignVueResolver({
-      //     }),
-      //   ],
-      // }),
+      Components({
+        resolvers: [AntDesignVueResolver({})],
+      }),
     ],
     build: {
       sourcemap: 'hidden',
