849187def2bb9eae11389c13ca00c48c402d973d
NguyenDuyHieu987 <vaicut6941@gmail.com>
2023-08-03 13:08:49 +0700

    add

--- plugins/antd.ts
+++ plugins/antd.ts
@@ -1,4 +1,5 @@
-import Antd, { ConfigProvider, Button } from 'ant-design-vue';
+// import Antd from 'ant-design-vue';
+import { Button } from 'ant-design-vue';
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.hook('app:created', () => {
