8c20d453b47ee8816169962df34492d96ed53e90
NguyenDuyHieu987 <vaicut6941@gmail.com>
2023-08-03 15:51:54 +0700

    add

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -156,7 +156,7 @@ export default defineNuxtConfig({
     url: 'https://phimhay247.tech',
   },
   css: [
-    // 'ant-design-vue/dist/antd.dark.min.css',
+    'ant-design-vue/dist/antd.dark.min.css',
     // 'element-plus/dist/index.css',
     '~/assets/style/globalStyle.scss',
     // '@fortawesome/fontawesome-svg-core/styles.css',
