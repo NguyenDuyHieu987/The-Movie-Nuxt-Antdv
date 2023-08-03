562c68202fba2b9bdc725cc6ff5c0f127cdd6650
NguyenDuyHieu987 <vaicut6941@gmail.com>
2023-08-03 12:41:56 +0700

    add

--- components/CastCrew/index.vue
+++ components/CastCrew/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="cast-crew">
-    <Tabs v-model:activeKey="activeTabCast">
-      <TabPane key="cast" tab="Diễn viên">
+    <a-tabs v-model:activeKey="activeTabCast">
+      <a-tab-pane key="cast" tab="Diễn viên">
         <SliderGroup
           v-show="loading"
           :data="dataCredit?.cast?.slice(0, 20)"
@@ -20,8 +20,8 @@
             </SwiperSlide>
           </template>
         </SliderGroup>
-      </TabPane>
-      <TabPane key="crew" tab="Đội ngũ" force-render>
+      </a-tab-pane>
+      <a-tab-pane key="crew" tab="Đội ngũ" force-render>
         <SliderGroup
           v-show="loading"
           :data="dataCredit?.crew?.slice(0, 20)"
@@ -40,13 +40,12 @@
             </SwiperSlide>
           </template>
         </SliderGroup>
-      </TabPane>
-    </Tabs>
+      </a-tab-pane>
+    </a-tabs>
   </div>
 </template>
 
 <script setup lang="ts">
-import { Tabs, TabPane } from 'ant-design-vue';
 import CastCard from '~/components/CastCard';
 import SliderGroup from '~/components/SliderGroup';
 import { getCredits } from '~/services/credit';
