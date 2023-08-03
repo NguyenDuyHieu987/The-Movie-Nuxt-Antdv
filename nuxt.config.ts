43ee92a93307e9377b174603c1bf83f0b3d10806
NguyenDuyHieu987 <vaicut6941@gmail.com>
2023-08-03 20:48:04 +0700

    add

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -154,16 +154,18 @@ export default defineNuxtConfig({
     url: 'https://phimhay247.tech',
   },
   css: [
-    // 'ant-design-vue/dist/antd.dark.min.css',
+    'ant-design-vue/dist/antd.dark.min.css',
     // 'element-plus/dist/index.css',
-    'ant-design-vue/es/menu/style/index.css',
-    'ant-design-vue/es/button/style/index.css',
-    'ant-design-vue/es/form/style/index.css',
-    'ant-design-vue/es/auto-complete/style/index.css',
-    'ant-design-vue/es/input/style/index.css',
-    'ant-design-vue/es/layout/style/index.css',
-    'ant-design-vue/es/select/style/index.css',
-    'ant-design-vue/es/dropdown/style/index.css',
+
+    // 'ant-design-vue/es/menu/style/index.css',
+    // 'ant-design-vue/es/button/style/index.css',
+    // 'ant-design-vue/es/form/style/index.css',
+    // 'ant-design-vue/es/auto-complete/style/index.css',
+    // 'ant-design-vue/es/input/style/index.css',
+    // 'ant-design-vue/es/layout/style/index.css',
+    // 'ant-design-vue/es/select/style/index.css',
+    // 'ant-design-vue/es/dropdown/style/index.css',
+
     '~/assets/style/fonts/GoogleFonts.css',
     '~/assets/style/globalStyle.scss',
     '~/assets/style/globalStyle/overwrite/antdv/antdv.scss',
@@ -340,24 +342,32 @@ export default defineNuxtConfig({
   components: {
     dirs: [
       {
-        // pathPrefix: false,
-        // extensions: ['.vue'],
+        pathPrefix: false,
+        extensions: ['.vue'],
       },
     ],
   },
   vite: {
     plugins: [
-      Components({
-        resolvers: [
-          AntDesignVueResolver({
-            // importStyle: false,
-            // resolveIcons: true,
-          }),
-        ],
-      }),
+      // Components({
+      //   resolvers: [
+      //     AntDesignVueResolver({
+      //       importStyle: 'less',
+      //     }),
+      //   ],
+      // }),
     ],
     ssr: {
       noExternal: ['moment', 'compute-scroll-into-view', 'ant-design-vue'],
+    },
+    css: {
+      preprocessorOptions: {
+        less: {
+          math: 'always',
+          relativeUrls: true,
+          javascriptEnabled: true,
+        },
+      },
     },
     build: {
       sourcemap: 'hidden',
