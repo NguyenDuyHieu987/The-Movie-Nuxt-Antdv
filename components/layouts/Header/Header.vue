6f7a4a7365069c511319e5a4d0d02007a1be3c1a
NguyenDuyHieu987 <vaicut6941@gmail.com>
2023-08-05 20:35:05 +0700

    add

--- components/layouts/Header/Header.vue
+++ components/layouts/Header/Header.vue
@@ -148,15 +148,80 @@
           </a-popover>
         </li>
 
-        <li class="notification">
+        <li v-if="isLogin" class="notification" :hide-timeout="250">
+          <el-dropdown trigger="click" popper-class="header notification">
+            <span class="el-dropdown-link notification">
+              <el-icon class="el-icon--right">
+                <a-badge :count="3" :overflow-count="9">
+                  <div class="show-notify-btn">
+                    <svg
+                      class="fa-bell fa-regular"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="2.2rem"
+                      height="2.2rem"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M12 1.996a7.49 7.49 0 0 1 7.496 7.25l.004.25v4.097l1.38 3.156a1.249 1.249 0 0 1-1.145 1.75L15 18.502a3 3 0 0 1-5.995.177L9 18.499H4.275a1.251 1.251 0 0 1-1.147-1.747L4.5 13.594V9.496c0-4.155 3.352-7.5 7.5-7.5ZM13.5 18.5l-3 .002a1.5 1.5 0 0 0 2.993.145l.007-.147ZM12 3.496c-3.32 0-6 2.674-6 6v4.41L4.656 17h14.697L18 13.907V9.509l-.003-.225A5.988 5.988 0 0 0 12 3.496Z"
+                      />
+                    </svg>
+
+                    <svg
+                      class="fa-bell fa-solid"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="2.2rem"
+                      height="2.2rem"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M9.042 19.003h5.916a3 3 0 0 1-5.916 0Zm2.958-17a7.5 7.5 0 0 1 7.5 7.5v4l1.418 3.16A.95.95 0 0 1 20.052 18h-16.1a.95.95 0 0 1-.867-1.338l1.415-3.16V9.49l.005-.25A7.5 7.5 0 0 1 12 2.004Z"
+                      />
+                    </svg>
+                  </div>
+                </a-badge>
+              </el-icon>
+            </span>
+            <template #dropdown>
+              <el-dropdown-menu class="dropdown-notification">
+                <div class="notification-header">
+                  <span> Thông báo</span>
+                </div>
+
+                <el-menu-item-group class="public" key="public">
+                  <template #title>
+                    <span>Thông báo chung</span>
+                    <span class="count-message">1</span>
+                  </template>
+                  <el-dropdown-item
+                    >Chào mừng bạn đến với Phimhay247
+                  </el-dropdown-item>
+                </el-menu-item-group>
+
+                <div class="separate" />
+
+                <el-menu-item-group class="private" key="private">
+                  <template #title>
+                    <span>Thông báo cá nhân</span>
+                    <span class="count-message">1</span>
+                  </template>
+                  <el-dropdown-item>Message 1</el-dropdown-item>
+                  <el-dropdown-item>Message 2</el-dropdown-item>
+                </el-menu-item-group>
+              </el-dropdown-menu>
+            </template>
+          </el-dropdown>
+        </li>
+
+        <!-- <li v-if="isLogin" class="notification" >
           <a-dropdown
-            v-if="isLogin"
             :trigger="['click']"
             class="dropdown-notification"
             overlayClassName="dropdown-notification header"
             placement="bottomRight"
           >
-            <span v-if="isLogin" class="ant-dropdown-link">
+            <span class="ant-dropdown-link">
               <a-badge :count="3" :overflow-count="9">
                 <div class="show-notify-btn">
                   <svg
@@ -215,46 +280,40 @@
               </el-menu>
             </template>
           </a-dropdown>
-        </li>
+        </li> -->
 
         <li class="account">
-          <a-dropdown
-            :trigger="['click']"
-            class="dropdown-account"
-            overlayClassName="dropdown-account header"
-            placement="bottomRight"
-          >
-            <span class="ant-dropdown-link">
-              <!-- <UserOutlined />
-                <CaretDownOutlined /> -->
+          <el-dropdown trigger="click" popper-class="header account">
+            <span class="el-dropdown-link account">
+              <el-icon class="el-icon--right">
+                <svg
+                  class="user-outlined"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="1.5rem"
+                  height="1.5rem"
+                  viewBox="0 0 1024 1024"
+                >
+                  <path
+                    d="M858.5 763.6a374 374 0 0 0-80.6-119.5a375.63 375.63 0 0 0-119.5-80.6c-.4-.2-.8-.3-1.2-.5C719.5 518 760 444.7 760 362c0-137-111-248-248-248S264 225 264 362c0 82.7 40.5 156 102.8 201.1c-.4.2-.8.3-1.2.5c-44.8 18.9-85 46-119.5 80.6a375.63 375.63 0 0 0-80.6 119.5A371.7 371.7 0 0 0 136 901.8a8 8 0 0 0 8 8.2h60c4.4 0 7.9-3.5 8-7.8c2-77.2 33-149.5 87.8-204.3c56.7-56.7 132-87.9 212.2-87.9s155.5 31.2 212.2 87.9C779 752.7 810 825 812 902.2c.1 4.4 3.6 7.8 8 7.8h60a8 8 0 0 0 8-8.2c-1-47.8-10.9-94.3-29.5-138.2zM512 534c-45.9 0-89.1-17.9-121.6-50.4S340 407.9 340 362c0-45.9 17.9-89.1 50.4-121.6S466.1 190 512 190s89.1 17.9 121.6 50.4S684 316.1 684 362c0 45.9-17.9 89.1-50.4 121.6S557.9 534 512 534z"
+                  />
+                </svg>
 
-              <svg
-                class="user-outlined"
-                xmlns="http://www.w3.org/2000/svg"
-                width="1.5rem"
-                height="1.5rem"
-                viewBox="0 0 1024 1024"
-              >
-                <path
-                  d="M858.5 763.6a374 374 0 0 0-80.6-119.5a375.63 375.63 0 0 0-119.5-80.6c-.4-.2-.8-.3-1.2-.5C719.5 518 760 444.7 760 362c0-137-111-248-248-248S264 225 264 362c0 82.7 40.5 156 102.8 201.1c-.4.2-.8.3-1.2.5c-44.8 18.9-85 46-119.5 80.6a375.63 375.63 0 0 0-80.6 119.5A371.7 371.7 0 0 0 136 901.8a8 8 0 0 0 8 8.2h60c4.4 0 7.9-3.5 8-7.8c2-77.2 33-149.5 87.8-204.3c56.7-56.7 132-87.9 212.2-87.9s155.5 31.2 212.2 87.9C779 752.7 810 825 812 902.2c.1 4.4 3.6 7.8 8 7.8h60a8 8 0 0 0 8-8.2c-1-47.8-10.9-94.3-29.5-138.2zM512 534c-45.9 0-89.1-17.9-121.6-50.4S340 407.9 340 362c0-45.9 17.9-89.1 50.4-121.6S466.1 190 512 190s89.1 17.9 121.6 50.4S684 316.1 684 362c0 45.9-17.9 89.1-50.4 121.6S557.9 534 512 534z"
-                />
-              </svg>
-
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="1.5rem"
-                height="1.5rem"
-                viewBox="0 0 1024 1024"
-              >
-                <path
-                  d="M840.4 300H183.6c-19.7 0-30.7 20.8-18.5 35l328.4 380.8c9.4 10.9 27.5 10.9 37 0L858.9 335c12.2-14.2 1.2-35-18.5-35z"
-                />
-              </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="1.5rem"
+                  height="1.5rem"
+                  viewBox="0 0 1024 1024"
+                >
+                  <path
+                    d="M840.4 300H183.6c-19.7 0-30.7 20.8-18.5 35l328.4 380.8c9.4 10.9 27.5 10.9 37 0L858.9 335c12.2-14.2 1.2-35-18.5-35z"
+                  />
+                </svg>
+              </el-icon>
             </span>
 
-            <template #overlay>
-              <a-menu class="dropdown-account">
-                <a-menu-item
+            <template #dropdown>
+              <el-dropdown-menu class="dropdown-account">
+                <el-dropdown-item
                   v-if="isLogin"
                   v-once
                   :class="{ active: isLogin }"
@@ -267,9 +326,11 @@
                       {{ userAccount?.email }}
                     </span>
                   </NuxtLink>
-                </a-menu-item>
+                </el-dropdown-item>
 
-                <a-menu-item
+                <div class="separate" />
+
+                <el-dropdown-item
                   v-if="isLogin && role == 'admin'"
                   v-once
                   key="dashboard"
@@ -277,23 +338,23 @@
                   <NuxtLink :to="$config.app.adminWebsiteUrl" target="_blank">
                     Bảng điều khiển
                   </NuxtLink>
-                </a-menu-item>
+                </el-dropdown-item>
 
-                <a-menu-item v-if="!isLogin" v-once key="login">
+                <el-dropdown-item v-if="!isLogin" v-once key="login">
                   <NuxtLink to="/login"> Đăng nhập</NuxtLink>
-                </a-menu-item>
+                </el-dropdown-item>
 
-                <a-menu-item
+                <el-dropdown-item
                   v-if="isLogin"
                   v-once
                   key="logout"
                   @click="handleLogout"
                 >
                   <span> Đăng xuất</span>
-                </a-menu-item>
-              </a-menu>
+                </el-dropdown-item>
+              </el-dropdown-menu>
             </template>
-          </a-dropdown>
+          </el-dropdown>
         </li>
       </ul>
     </div>
