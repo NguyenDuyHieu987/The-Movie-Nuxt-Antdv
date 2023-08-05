a9e99b3e1ccac5a0af5930f7dbd8c25eb5239c4c
NguyenDuyHieu987 <vaicut6941@gmail.com>
2023-08-05 21:47:16 +0700

    add

--- components/layouts/Header/Header.vue
+++ components/layouts/Header/Header.vue
@@ -108,42 +108,31 @@
                 <span> Tìm kiếm </span>
               </h3>
 
-              <a-auto-complete
-                v-model:value="valueInput"
+              <a-input-search
                 class="search-popover"
-                popupClassName="certain-category-search-dropdown"
-                :options="dataSearch"
-                style="width: 100%"
-                :open="isOpenAutoComplete"
+                placeholder="Nhập tên phim để tìm kiếm..."
+                size="large"
+                allowClear
+                bordered
+                :loading="loadingSearch"
+                @search="handleSearch"
                 @change="handleChangeInput(valueInput)"
-                @focus="isOpenAutoComplete = true"
-                @blur="isOpenAutoComplete = false"
               >
-                <a-input-search
-                  class="search-popover"
-                  placeholder="Nhập tên phim để tìm kiếm..."
-                  size="large"
-                  allowClear
-                  bordered
-                  :loading="loadingSearch"
-                  @search="handleSearch"
-                >
-                  <template #enterButton>
-                    <svg
-                      class="fa-magnifying-glass"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="1.8rem"
-                      height="1.8rem"
-                      viewBox="0 0 512 512"
-                    >
-                      <path
-                        fill="currentColor"
-                        d="M416 208c0 45.9-14.9 88.3-40 122.7l126.6 126.7c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0s208 93.1 208 208zM208 352a144 144 0 1 0 0-288a144 144 0 1 0 0 288z"
-                      />
-                    </svg>
-                  </template>
-                </a-input-search>
-              </a-auto-complete>
+                <template #enterButton>
+                  <svg
+                    class="fa-magnifying-glass"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="1.8rem"
+                    height="1.8rem"
+                    viewBox="0 0 512 512"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M416 208c0 45.9-14.9 88.3-40 122.7l126.6 126.7c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0s208 93.1 208 208zM208 352a144 144 0 1 0 0-288a144 144 0 1 0 0 288z"
+                    />
+                  </svg>
+                </template>
+              </a-input-search>
             </template>
           </a-popover>
         </li>
