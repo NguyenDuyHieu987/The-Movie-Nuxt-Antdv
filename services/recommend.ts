af587e9bdd9fe1482bbf958961da262aca4289f5
NguyenDuyHieu987 <vaicut6941@gmail.com>
2023-08-21 14:25:27 +0700

    add

--- services/recommend.ts
+++ services/recommend.ts
@@ -2,5 +2,5 @@ import { makeRequest } from './makeRequest';
 import { getWithExpiry } from '@/utils/customlocalStorage';
 
 export function getMyRecommend(page: number = 1, limit: number = 6) {
-  return makeRequest(`/recommend/getrecommend?page=${page}&limit=${limit}`);
+  return makeRequest(`/recommend/get?page=${page}&limit=${limit}`);
 }
