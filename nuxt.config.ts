9c17a4401113a1a2b0d84545538d5fd7132836c1
NguyenDuyHieu987 <104075342+NguyenDuyHieu987@users.noreply.github.com>
2023-11-16 00:33:21 +0700

    add

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -193,7 +193,7 @@ export default defineNuxtConfig({
       },
     },
     output: {
-      dir: 'D:\\MyWebsite\\Phimhay247\\.output',
+      // dir: 'D:\\MyWebsite\\Phimhay247\\.output',
     },
   },
   vite: {
