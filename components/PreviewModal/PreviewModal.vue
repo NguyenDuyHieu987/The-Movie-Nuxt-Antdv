6bd5f52ab2b1109b6606b44fc804960c59e922d8
NguyenDuyHieu987 <104075342+NguyenDuyHieu987@users.noreply.github.com>
2023-12-01 17:11:07 +0700

    add

--- components/PreviewModal/PreviewModal.vue
+++ components/PreviewModal/PreviewModal.vue
@@ -10,7 +10,6 @@
           'only-right': isOnlyRight,
           'show-video': showVideo,
         }"
-        @click.prevent="onClickPreviewModal"
         :style="`--dominant-backdrop-color: ${item.dominant_backdrop_color[0]}, ${item.dominant_backdrop_color[1]},${item.dominant_backdrop_color[2]}`"
       >
         <el-skeleton :loading="loading" animated>
@@ -56,6 +55,40 @@
                   autoplay
                   preload="metadata"
                 ></video>
+
+                <div class="video-tool">
+                  <div class="volume">
+                    <svg
+                      v-show="!videoStates.isVolumeOff"
+                      name="ic:baseline-volume-up"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="2rem"
+                      height="2rem"
+                      viewBox="0 0 24 24"
+                      @click="onClickVolumeUp"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M3 9v6h4l5 5V4L7 9H3zm13.5 3A4.5 4.5 0 0 0 14 7.97v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"
+                      />
+                    </svg>
+
+                    <svg
+                      v-show="videoStates.isVolumeOff"
+                      name="ic:baseline-volume-off"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="2rem"
+                      height="2rem"
+                      viewBox="0 0 24 24"
+                      @click="onClickVolumeOff"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M16.5 12A4.5 4.5 0 0 0 14 7.97v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51A8.796 8.796 0 0 0 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27L7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06a8.99 8.99 0 0 0 3.69-1.81L19.73 21L21 19.73l-9-9L4.27 3zM12 4L9.91 6.09L12 8.18V4z"
+                      />
+                    </svg>
+                  </div>
+                </div>
               </div>
 
               <div
@@ -66,7 +99,7 @@
               <div v-if="isInHistory" class="viewed-overlay-bar"></div>
             </div>
 
-            <div class="bottom-content">
+            <div class="bottom-content" @click="onClickPreviewModal">
               <div class="widget">
                 <div class="left">
                   <el-tooltip
@@ -398,6 +431,9 @@ const videoSrc = computed<string>(
   // 'http://localhost:5002/videos' + '/feature/Transformer_5'
   // + '.m3u8'
 );
+const videoStates = reactive({
+  isVolumeOff: false,
+});
 
 onMounted(() => {
   // window.addEventListener('pointermove', (e: any) => {
@@ -730,6 +766,15 @@ const handelAddToList = (e: any) => {
 };
 
 const getPopupContainer = () => document.querySelector('.preview-modal');
+
+const onClickVolumeUp = () => {
+  videoStates.isVolumeOff = true;
+  video.value!.muted = true;
+};
+const onClickVolumeOff = () => {
+  videoStates.isVolumeOff = false;
+  video.value!.muted = false;
+};
 </script>
 
 <style lang="scss" src="./PreviewModal.scss"></style>
