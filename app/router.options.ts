acb7dee2a510e9adbff08fc83f207a8024bfadc5
NguyenDuyHieu987 <104075342+NguyenDuyHieu987@users.noreply.github.com>
2023-12-01 17:12:40 +0700

    add

--- app/router.options.ts
+++ app/router.options.ts
@@ -3,7 +3,7 @@ import type { RouterConfig } from '@nuxt/schema';
 export default <RouterConfig>{
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
-      window.history.scrollRestoration = 'auto';
+      // window.history.scrollRestoration = 'auto';
       return { ...savedPosition, behavior: 'instant' };
     } else {
       return {
