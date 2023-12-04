75bc56598a000236ccb64d1aa767a50015b044d1
NguyenDuyHieu987 <104075342+NguyenDuyHieu987@users.noreply.github.com>
2023-12-05 00:15:56 +0700

    add

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -84,7 +84,7 @@ export default defineNuxtConfig({
     restoreState: true,
     headNext: true,
     asyncContext: true,
-    // inlineSSRStyles: false,
+    inlineSSRStyles: true,
     defaults: {
       useAsyncData: {
         deep: true,
