70d1aaff8d29b5c7fa708012d705b6f1d8578a01
NguyenDuyHieu987 <104075342+NguyenDuyHieu987@users.noreply.github.com>
2023-12-10 22:54:19 +0700

    add

--- services/authentication.ts
+++ services/authentication.ts
@@ -72,6 +72,7 @@ export function signUp(params: { otp: string; vrfSignupToken: string }) {
     method: 'POST',
     // headers: headers,
     data: bodyFormData,
+    getResponseHeaders: true,
   });
 }
 
