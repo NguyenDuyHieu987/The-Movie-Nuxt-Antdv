c587e582da932c7fa1ca84c424f3c87f8053c845
NguyenDuyHieu987 <104075342+NguyenDuyHieu987@users.noreply.github.com>
2023-12-20 12:48:05 +0700

    add

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -122,7 +122,7 @@ export default defineNuxtConfig({
     'nuxt-lazy-load',
     '@nuxtjs/fontaine',
     'nuxt-gtag',
-
+    'nuxt-delay-hydration',
     // SEO
     'nuxt-simple-sitemap',
   ],
@@ -190,6 +190,10 @@ export default defineNuxtConfig({
     observerConfig: {},
   },
   fontMetrics: {},
+  delayHydration: {
+    debug: !isProduction,
+    mode: 'init',
+  },
   // SEO
   site: {
     url: 'https://phimhay247z.org',
